c283cf06014595e8c13883cf3f14e6fc9636f852
Dani Bachs <danibachsl@protonmail.com>
2022-11-29 12:28:42 +0100

    index.js from pages modified

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -336,7 +336,7 @@ export default function Home() {
         </div>
       </div>
 
-      <footer className={styles.footer}>Made with &#10084; by Your Name</footer>
+      <footer className={styles.footer}>Made with &#10084; by Dani Bachs Lobo</footer>
     </div>
   );
 }
